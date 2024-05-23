a264ab55e19491de1a5904fdcc6ac754c7b82eea
SouravDeyGithub <148003047+SouravDeyGithub@users.noreply.github.com>
2024-05-23 21:18:41 +0530

    Add files via upload

---
+++ Program-3.c
@@ -0,0 +1,18 @@
+//C program to print reversed of a number
+#include <stdio.h>
+
+int main() {
+    int num, reversedNum = 0, remainder;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        remainder = num % 10;
+        reversedNum = reversedNum * 10 + remainder;
+        num /= 10;
+    }
+    printf("Reversed number: %d\n", reversedNum);
+
+    return 0;
+}
