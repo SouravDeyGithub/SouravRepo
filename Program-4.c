de424ae05ced94aba429c7b436695aa3314f578a
SouravDeyGithub <148003047+SouravDeyGithub@users.noreply.github.com>
2024-05-23 21:19:05 +0530

    Add files via upload

---
+++ Program-4.c
@@ -0,0 +1,41 @@
+//C program to create a simple calculator
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    switch(operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if(num2 != 0)
+                result = num1 / num2;
+            else {
+                printf("Error! Division by zero.\n");
+                return 1;
+            }
+            break;
+        default:
+            printf("Error! Invalid operator.\n");
+            return 1;
+    }
+
+    printf("Result: %lf\n", result);
+
+    return 0;
+}
