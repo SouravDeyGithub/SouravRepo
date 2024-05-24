936eb83c6c47d85dfbc7447e8b2ee48e5022a9cf
SouravDeyGithub <148003047+SouravDeyGithub@users.noreply.github.com>
2024-05-24 10:50:51 +0530

    Add files via upload

---
+++ Size.of.datatype.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+main()
+{
+   printf ("No. of Bytes occupied by int is : %d \n", sizeof(int));
+   printf ("No. of Bytes occupied by float is : %d \n", sizeof(float));
+   printf ("No. of Bytes occupied by double is : %d \n", sizeof(double));
+   printf ("No. of Bytes occupied by char is : %d \n", sizeof(char));
+   getch();
+
+}
+
