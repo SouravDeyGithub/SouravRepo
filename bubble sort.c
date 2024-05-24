18ae4b19c33cb0e6c001db04b1a53a3dc4649d45
SouravDeyGithub <148003047+SouravDeyGithub@users.noreply.github.com>
2024-05-24 11:49:33 +0530

    Add files via upload

---
+++ bubble sort.c
@@ -0,0 +1,29 @@
+//To sort an array of integers using the bubble sort algorithm and then print the sorted array
+#include <stdio.h>
+
+int main() {
+    int arr[] = {3, 2, 4, 9, 1, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
