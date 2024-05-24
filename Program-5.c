9be6ed7dbde0a7ff38623ac37a67e0b194ba6fe8
SouravDeyGithub <148003047+SouravDeyGithub@users.noreply.github.com>
2024-05-24 18:17:21 +0530

    Add files via upload

---
+++ Program-5.c
@@ -0,0 +1,30 @@
+///C program to check whether a number is Prime or not
+
+#include <stdio.h>
+int isPrime(int n) {
+    if (n < 2) {
+        return 0;
+    }
+
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isPrime(num)) {
+        printf("%d is a prime number.\n", num);
+    } else {
+        printf("%d is not a prime number.\n", num);
+    }
+
+    return 0;
+}
